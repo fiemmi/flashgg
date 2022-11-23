3db388a0583cf96dada6586d8db1b7359c750f24
Fabio Iemmi <fabio.iemmi@cern.ch>
2022-11-23 14:43:37 +0100

    add variables to output miniTrees

--- Systematics/python/SystematicDumperDefaultVariables.py
+++ Systematics/python/SystematicDumperDefaultVariables.py
@@ -4,13 +4,36 @@ minimalVariables = ["CMS_hgg_mass[160,100,180]:=diPhoton().mass",
                                                                                #when doing systematics, variables need to have a binning
                                                                                #specified, otherwise the rooDataHist end up empty.
             								       #an assert in the code prevents you from doing this.
-                    "centralObjectWeight[1,-999999.,999999.] := centralWeight"]
+                    "centralObjectWeight[1,-999999.,999999.] := centralWeight",
+                    "diphoMVA[200,-1,1] :=diPhotonMVA().result",
+                    "leadE := diPhoton.leadingPhoton().energy",
+                    "subleadE := diPhoton.subLeadingPhoton().energy",
+                    "leadPt :=diPhoton().leadingPhoton.pt",
+                    "subleadPt :=diPhoton().subLeadingPhoton.pt",
+                    "leadmva  := diPhotonMVA.leadmva",
+                    "subleadmva := diPhotonMVA.subleadmva",
+                    "sigmarv := diPhotonMVA.sigmarv",
+                    "sigmawv := diPhotonMVA.sigmawv",
+                    "maxEta :=max(abs(diPhoton().leadingPhoton.superCluster.eta),abs(diPhoton().leadingPhoton.superCluster.eta))",
+                    "genZ :=tagTruth().genPV().z",
+                    "vtxZ :=diPhoton().vtx().z"]
 
 minimalHistograms = []
 
 minimalNonSignalVariables = ["CMS_hgg_mass[160,100,180]:=diPhoton().mass",
                              "sigmaRV:=diPhotonMVA().sigmarv",
-                             "sigmaMoM_decorr:=diPhotonMVA().decorrSigmarv"]#,"centralObjectWeight[1,-999999.,999999.] := centralWeight"]
+                             "sigmaMoM_decorr:=diPhotonMVA().decorrSigmarv",
+                             "diphoMVA[200,-1,1] :=diPhotonMVA().result",
+                             "leadE := diPhoton.leadingPhoton().energy",
+                             "subleadE := diPhoton.subLeadingPhoton().energy",
+                             "leadPt :=diPhoton().leadingPhoton.pt",
+                             "subleadPt :=diPhoton().subLeadingPhoton.pt",
+                             "leadmva  := diPhotonMVA.leadmva",
+                             "subleadmva := diPhotonMVA.subleadmva",
+                             "sigmarv := diPhotonMVA.sigmarv",
+                             "sigmawv := diPhotonMVA.sigmawv",
+                             "maxEta :=max(abs(diPhoton().leadingPhoton.superCluster.eta),abs(diPhoton().leadingPhoton.superCluster.eta))",
+                             "vtxZ :=diPhoton().vtx().z"]#,"centralObjectWeight[1,-999999.,999999.] := centralWeight"]
 
 minimalVariablesHTXS = minimalVariables+["stage0bin[72,9.5,81.5] := tagTruth().HTXSstage0bin"]
 
