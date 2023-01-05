7e1abe2c0f6222993112011387dfbc71bd742f7e
Fabio Iemmi <fabio.iemmi@cern.ch>
2023-01-05 08:56:08 +0100

    some (almost) irrelevant changes to executable files for running jobs

--- Systematics/UL2017/run_Sig125_2017_4Untag.sh
+++ Systematics/UL2017/run_Sig125_2017_4Untag.sh
@@ -3,7 +3,8 @@
 #queue="longlunch"
 queue="workday"  #"longlunch"
 #
-fggRunJobs.py --load sig_UL2017.json -d Sig125_4Unatg -x cmsRun workspaceStd_OnlyUntag.py maxEvents=-1 -n 100 -q ${queue} -H -D -P  --no-use-tarball  --no-copy-proxy dumpWorkspace=False dumpTrees=True  doSystematics=True useParentDataset=True doPdfWeights=False
- 
- 
+fggRunJobs.py --load sig_UL2017.json -d Sig125_4Unatg_retrainBDT_updatedUncs -x cmsRun workspaceStd_OnlyUntag.py maxEvents=-1 -n 100 -q ${queue} -H -D -P  --no-use-tarball  --no-copy-proxy dumpWorkspace=False dumpTrees=True  doSystematics=True useParentDataset=False doPdfWeights=False
 
+#FIemmi: set useParentDataset to False if you don't want to load the parent MiniAOD file to recalcuate the thoery uncertainties 
+#Fiemmi: set doSystematics=False if you don't care about the systematic part (e.g., you only want to check the statystical error on the Higgs mass) 
+#Fiemmi: set copyInputMicroAOD=1. Instead of doing sth like root -l root://cms-xrd-global.cern.ch///path/to/file.root, do instead xrdcp root://cms-xrd-global.cern.ch///path/to/file.root ./file.root; root -l ./file.root
--- Systematics/UL2017/run_data_UL2017_4Untag.sh
+++ Systematics/UL2017/run_data_UL2017_4Untag.sh
@@ -1,2 +1,2 @@
 queue="workday"
-fggRunJobs.py --load UL_runII_v1_2017.json -d DataUL2017_4Untag  -x cmsRun workspaceStd_OnlyUntag.py maxEvents=-1 -q ${queue} -n 100 --no-copy-proxy --no-use-tarball  dumpWorkspace=False dumpTrees=True 
+fggRunJobs.py --load UL_runII_v1_2017.json -d DataUL2017_4Untag_retrainBDT_updatedUncs  -x cmsRun workspaceStd_OnlyUntag.py maxEvents=-1 -q ${queue} -n 200 --no-copy-proxy --no-use-tarball  dumpWorkspace=False dumpTrees=True 
