152a50a0c1d27f90a3ff1a188ad9150a73dae389
Fabio Iemmi <fabio.iemmi@cern.ch>
2023-03-31 15:03:56 +0200

    small changes to UL16 executable files for job submission

--- Systematics/UL2016/preVFP/run_ZH125_2016PreVFP_Summer20UL_4Untag.sh
+++ Systematics/UL2016/preVFP/run_ZH125_2016PreVFP_Summer20UL_4Untag.sh
@@ -2,7 +2,7 @@
 #espresso, microcentury, longlunch, workday, tomorrow, testmatch, nextweek
 queue="workday"  #"longlunch"
 #
-fggRunJobs.py --load ZH125_preVFP_Summer20UL.json -d ZH125_2016PreVFP_Summer20UL -x cmsRun workspaceStd_OnlyUntag.py maxEvents=-1 -n 200 -q ${queue} -H -D -P  --no-use-tarball  --no-copy-proxy  dumpWorkspace=False dumpTrees=True  MiniTreeOnly=True
+fggRunJobs.py --load ZH125_preVFP_Summer20UL.json -d ZH125_2016PreVFP_Summer20UL_quater -x cmsRun workspaceStd_OnlyUntag.py maxEvents=-1 -n 200 -q ${queue} -H -D -P  --no-use-tarball  --no-copy-proxy  dumpWorkspace=False dumpTrees=True doSystematics=True useParentDataset=False doPdfWeights=False
 
  
 
